82903444ff5fc696130434c9ae8282d62597f405
NikiDevelop <niki93g@hotmail.com>
2023-09-08 13:14:25 +0200

    update genders

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path("", views.renderPosts, name="posts"),
-    path("<int:post_id>", views.post_detail, name="post_detail"),
+    path("post/<int:post_id>", views.post_detail, name="post_detail"),
+    
+
 ]
