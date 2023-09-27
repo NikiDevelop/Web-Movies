6151bf7a6102c610a59c0803366db5de1cf16a22
Nikifor Genchev <105102619+NikiDevelop@users.noreply.github.com>
2023-09-27 21:42:09 +0200

    Update settings.py

--- ProyectoMovie/settings.py
+++ ProyectoMovie/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fu&h#-79s405dk1f*ljf&(wz04*2*!xm^xl80^+a^ol80)t_&@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-movies.azurewebsites.net']
 
